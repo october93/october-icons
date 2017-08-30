50e678eedeafc301c4b6bf56d178c13d96e369cb
Christopher Gilson <c.gilson11@gmail.com>
2017-08-30 14:26:31 -0700

    BLAP BLAP

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
-echo "setting up git"
+if ["$COMMITTER_EMAIL" != "$DEVOPS_EMAIL"]; then
 
-git config --global user.email $DEVOPS_EMAIL
-git config --global user.name "October DevOps"
+  echo "setting up git"
 
-echo "push changes to https://github.com/${TRAVIS_REPO_SLUG}"
+  git config --global user.email $DEVOPS_EMAIL
+  git config --global user.name "October DevOps"
 
-# Travis operates in a detached mode. Harmonize travis commits in current branch
-head_ref=$(git rev-parse HEAD)
-git rebase $TRAVIS_BRANCH
-git checkout $TRAVIS_BRANCH
-git merge --ff-only $head_ref
-echo "building..."
+  echo "push changes to https://github.com/${TRAVIS_REPO_SLUG}"
 
-yarn generate
-echo "committing..."
+  # Travis operates in a detached mode. Harmonize travis commits in current branch
+  head_ref=$(git rev-parse HEAD)
+  git rebase $TRAVIS_BRANCH
+  git checkout $TRAVIS_BRANCH
+  git merge --ff-only $head_ref
+  echo "building..."
 
-git commit -am "auto-build"
+  yarn generate
+  echo "committing..."
 
-npm version patch
-git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $TRAVIS_BRANCH --follow-tags > /dev/null 2>&1
+  git commit -am "auto-build"
 
-echo "patching version.."
+  npm version patch
+  git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $TRAVIS_BRANCH --follow-tags > /dev/null 2>&1
+
+  echo "patching version.."
+
+fi
