2a56c7227633005ccd9023ee3501face3aba9e0c
Christopher Gilson <c.gilson11@gmail.com>
2017-08-30 14:08:54 -0700

    TURVUS

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,21 @@ echo "setting up git"
 git config --global user.email $DEVOPS_EMAIL
 git config --global user.name "October DevOps"
 
-echo "patching version.."
+echo "push changes to https://github.com/${TRAVIS_REPO_SLUG}"
+
+# Travis operates in a detached mode. Harmonize travis commits in current branch
+head_ref=$(git rev-parse HEAD)
+git rebase $TRAVIS_BRANCH
+git checkout $TRAVIS_BRANCH
+git merge --ff-only $head_ref
+echo "building..."
+
+yarn generate
+echo "committing..."
+
+git commit -am "auto-build"
 
 npm version patch
+git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $TRAVIS_BRANCH --follow-tags > /dev/null 2>&1
+
+echo "patching version.."
