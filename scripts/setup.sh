095e8e81b682afce9fedf7f5395c6e5795b54c1a
Chris Gilson <c.gilson11@gmail.com>
2018-09-22 14:01:32 -0700

    crash pt 2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,6 @@ echo "setting up git"
 git config --global user.email $DEVOPS_EMAIL
 git config --global user.name "October DevOps"
 
-yarn generate && yarn copy-svgs
-
 echo "patching version.."
 
 npm version patch
