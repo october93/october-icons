e81220bede371feca811f66503a9e5bfd35c997b
Chris Gilson <c.gilson11@gmail.com>
2018-09-22 13:56:52 -0700

    intentional fail

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,8 @@ echo "setting up git"
 git config --global user.email $DEVOPS_EMAIL
 git config --global user.name "October DevOps"
 
+yarn generate && yarn copy-svgs
+
 echo "patching version.."
 
 npm version patch
