d69dbb7e4e13a1d4e7cb65de78ea067e94097656
Chris Gilson <c.gilson11@gmail.com>
2019-09-06 20:04:00 -0700

    Open source October

---
+++ scripts/push.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "push changes to https://github.com/${TRAVIS_REPO_SLUG}"
+
+# Travis operates in a detached mode. Harmonize travis commits in current branch
+head_ref=$(git rev-parse HEAD)
+git rebase $TRAVIS_BRANCH
+git checkout $TRAVIS_BRANCH
+git merge --ff-only $head_ref
+
+git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $TRAVIS_BRANCH --follow-tags > /dev/null 2>&1
---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "setting up git"
+
+git config --global user.email $DEVOPS_EMAIL
+git config --global user.name "October DevOps"
+
+echo "patching version.."
+
+npm version patch
